7ef7558497fe8fe7771d646add879860533e2642
sheryl <sheirawadigau@gmail.com>
2024-07-15 17:49:04 +0800

    connect to Mongodb

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
+//env
+const{ DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
 
+//error mongodb
+mongoose.connection.on('error', (err)=>{
+    logger.error(`mongodb connection error : ${err}`);
+    process.exit(1);
+});
 
-let server = app.listen(PORT,()=>{
+//mongodb debug
+if(process.env.NODE_ENV !== "production"){
+    mongoose.set("debug", true);
+}
+
+//mongodb connection
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    logger.info('connected to mongodb');
+})
+
+let server;
+
+server = app.listen(PORT,()=>{
     logger.info(`server listening at ${PORT}`);
 });
 
 //handle error
-
 const exitHandler = () => {
     if(server){
         logger.info("Server closed.");
