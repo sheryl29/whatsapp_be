a35254b725f2ad5598470a538eebaa5bf97c3c7b
sheryl <sheirawadigau@gmail.com>
2024-09-04 21:02:02 +0800

    group chat

--- src/routes/conversation.route.js
+++ src/routes/conversation.route.js
@@ -3,7 +3,7 @@ import trimRequest from "trim-request";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 import {
-  //createGroup,
+  createGroup,
   create_open_conversation,
   getConversations,
 } from "../controllers/conversation.controller.js";
@@ -14,6 +14,6 @@ router
   .route("/")
   .post(trimRequest.all, authMiddleware, create_open_conversation);
 router.route("/").get(trimRequest.all, authMiddleware, getConversations);
-//router.route("/group").post(trimRequest.all, authMiddleware, createGroup);
+router.route("/group").post(trimRequest.all, authMiddleware, createGroup);
 
 export default router;
